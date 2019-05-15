2724f404e9472ae55ce75b30f7223ce04afa316f
yidahc <yidah.a.curiel@gmail.com>
2019-05-15 18:16:27 -0500

    finished setting up boilerplate

--- database-mysql/index.js
+++ database-mysql/index.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : 'FILL_ME_IN',
+  password : '170482',
   database : 'test'
 });
 
