e52b3e04b5b3c2027a2dffe470d4e9f7f098aea0
yidahc <yidah.a.curiel@gmail.com>
2019-05-18 20:18:31 -0500

    connecting to mongo.db database

--- database-mongo/models/user.js
+++ database-mongo/models/user.js
@@ -1,10 +1,5 @@
-var mongoose = require('mongoose');
-require('dotenv').config();
-
-mongoose.Promise = global.Promise;
-mongoose.connect(process.env.DATABASE)
-
-const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt'); // used to hash passwords
 const SALT_I = 10;
 
 const userSchema = mongoose.Schema({
@@ -12,12 +7,12 @@ const userSchema = mongoose.Schema({
       type: String,
       required: true,
       trim: true,
-      unique: 1
+      unique: 1 // only one user per email, 1 is for true
     },
     password: {
       type: String,
       required: true,
-      minLength: 5
+      minLength: 5 // password must be a string at least 5 characters long and is required
     },
     name: {
       type: String,
@@ -35,26 +30,26 @@ const userSchema = mongoose.Schema({
     },
     history: {
       type:Array,
-      default:[]
+      default:[] 
     },
     role: {
       type: Number,
-      default: 0
+      default: 0 // either you are an administrator or a user, 0 is for user
     },
     token: {
       type: Number
     }
   });
   
-  userSchema.pre('save', function(next){
+  userSchema.pre('save', function(next){ // next is function in server/config that will run after this function
     var user = this; //es5 requires us to specify what 'this' is since it is not automatically 
     //bound at runtime
-    bcrypt.genSalt(SALT_I, function(err, salt){
-      if (err) return next(err);
+    bcrypt.genSalt(SALT_I, function(err, salt){ // generating hash for user password
+      if (err) return next(err); // if error, continue to next function with err as parameter
       bcrypt.hash(user.password, salt, function(err, hash){ //with es6, one would write "this.password"
-        if (err) return next(err);
-        user.password = hash;
-        next();
+        if (err) return next(err); // next function will return "success: false"
+        user.password = hash; // generating hash for user password
+        next(); // continue to next function
       });
     });
   })
--- server/config.js
+++ server/config.js
@@ -4,7 +4,8 @@ const compression = require('compression');
 const route = require('../server/routes.js')
 // UNCOMMENT THE DATABASE YOU'D LIKE TO USE
 const app = express();
-// var items = require('../database-mongo');
+
+const { User } = require('../database-mongo');
 
 // UNCOMMENT FOR REACT
 app.use(express.static(__dirname + '/../react-client/dist'));
@@ -15,10 +16,19 @@ app.use(bodyParser.urlencoded({extended: false}));
 // app.use(express.static(__dirname + '/../angular-client'));
 // app.use(express.static(__dirname + '/../node_modules'));
 
-// react-router fallback so we can reload without visiting root
-
-app.get('/*', route.fallback);
+app.get('/*', route.fallback); // react-router fallback so we can reload without visiting root
 
 app.get('/items', route.getItems);
+
+app.post('/api/users/register', (req, res)=>{
+    const user = new User(req.body)
+    user.save((err,doc)=>{
+      if (err) return res.json({success:false,err});
+      res.status(200).json({
+          success: true,
+          userdata: doc
+      })
+    })
+})
 
 module.exports = app;
