02db4cd7b03568bdc3bdc590b58be8c5ea16f2bb
yidahc <yidah.a.curiel@gmail.com>
2019-06-04 12:20:32 -0500

    added cards to product component and rendering dummy data

--- react-client/src/components/Products.jsx
+++ react-client/src/components/Products.jsx
@@ -1,18 +1,36 @@
 import React from 'react';
 import { NavLink} from 'react-router-dom';
 
-//import Products from './Products.jsx';
+import ProductCards from '../utils/ProductCards.jsx';
 
 class Products extends React.Component {
   constructor(props) {
     super(props);
     this.state = { 
-      data: []
-    }
+      data: [
+      {
+        id: 1,
+        name: 'Eli',
+        description: 'Really pretty girl',
+        price: 50000,
+        images: ['./images/slide7.jpg'],
+      },
+      {
+        id: 2,
+        name: 'Long Wig',
+        description: 'Long wig for girl',
+        price: 350,
+        images: ['./images/slide6.jpg'],
+      },     
+    ]
   }
+}
+
+
   render () {
     return (  
       <div>I am the Products component
+        <ProductCards list={this.state.data}/>
       </div>
     )
   }
